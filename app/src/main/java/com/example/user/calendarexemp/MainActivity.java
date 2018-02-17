3f4933872435f83952ce43b2c6ca2bcbbd740a39
Pavel Dolliner <dolliner.pavel@gmail.com>
2018-02-18 01:23:42 +0300

    1commit

--- app/src/main/java/com/example/user/calendarexemp/MainActivity.java
+++ app/src/main/java/com/example/user/calendarexemp/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.user.calendarexemp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.Arrays;
 
@@ -11,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
     String[] data = new String[42];
     String[] data1 = new String[42];
     String[] data2 = new String[42];
+    ViewPager vpMain;
+    PagerAdapter pagerAdapter;
+    TextView textView4;
+    ImageButton imageButton;
+    ImageButton imageButton2;
 
+    static final int PAGE_COUNT = 12;
+    MonthFragment monthFragment;
     GridView gvMain;
     ArrayAdapter<String> adapter;
 
@@ -19,24 +35,84 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Arrays.fill(data,"");
-        Arrays.fill(data1,"");
-        Arrays.fill(data2,"");
-        int j = 1;
-        for (int i = 5; i < 36; i++) {
-            data[i] = String.valueOf(j);
-            data1[i] = "00:00";
-            data2[i] = "4";
-            j++;
-        }
-        Adapter adapter = new Adapter(this, data, data1, data2);
-        gvMain = (GridView) findViewById(R.id.gvMain);
-        gvMain.setAdapter(adapter);
-        adjustGridView();
 
-    }//onCreate
+        textView4 = findViewById(R.id.textView4);
+        imageButton = findViewById(R.id.imageButton);
+        imageButton2 = findViewById(R.id.imageButton2);
+//        Arrays.fill(data,"");
+//        Arrays.fill(data1,"");
+//        Arrays.fill(data2,"");
+//        int j = 1;
+//        for (int i = 5; i < 36; i++) {
+//            data[i] = String.valueOf(j);
+//            data1[i] = "00:00";
+//            data2[i] = "4";
+//            j++;
+//        }
 
-    private void adjustGridView() {
-        gvMain.setNumColumns(7);//numColumns – кол-во столбцов в сетке
+        //создаем экземпляр объекта MonthFragment
+        monthFragment = new MonthFragment();
+//        Bundle args = new Bundle();    // объект для передачи параметров в диалог
+//        //передаем данные об  авторизированном пользователе
+//        args.putStringArray("data", data);
+//        args.putStringArray("data1", data1);
+//        args.putStringArray("data2", data2);
+//        monthFragment.setArguments(args);
+
+        vpMain = (ViewPager) findViewById(R.id.vpMain);
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        vpMain.setAdapter(pagerAdapter);
+
+        vpMain.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d("MyLog", "onPageSelected, position = " + position);
+                textView4.setText(String.valueOf(position));
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vpMain.setCurrentItem(vpMain.getCurrentItem()-1);
+            }
+        });
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vpMain.setCurrentItem(vpMain.getCurrentItem()+1);
+            }
+        });
     }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public MonthFragment getItem(int position) {
+
+            return monthFragment.newInstance( position, data, data1, data2);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+    }
+
 }//MainActivity
